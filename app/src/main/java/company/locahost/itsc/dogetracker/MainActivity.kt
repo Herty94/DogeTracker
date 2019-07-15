70a48aad73a7feeee56f64453b84d762cf08f1ac
Herty94 <da.hertneky@gmail.com>
2019-07-15 13:30:56 +0200

    Zakladná obrazovka Update

--- app/src/main/java/company/locahost/itsc/dogetracker/MainActivity.kt
+++ app/src/main/java/company/locahost/itsc/dogetracker/MainActivity.kt
@@ -1,15 +1,34 @@
 package company.locahost.itsc.dogetracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import company.locahost.itsc.dogetracker.login.Login
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btLogin: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btLogin.findViewById<Button>(R.id.bt_login)
+
+        btLogin.setOnClickListener (object: View.OnClickListener {
+           override fun onClick(view:View): Unit{
+               val intent = Intent(this@MainActivity, Login::class.java )
+               finish()
+               startActivity(intent)
+           }
+
+        })
+
+
 
 
     }
--- app/src/main/java/company/locahost/itsc/dogetracker/login/Login.kt
+++ app/src/main/java/company/locahost/itsc/dogetracker/login/Login.kt
@@ -1,11 +1,10 @@
-package company.locahost.itsc.dogetracker
+package company.locahost.itsc.dogetracker.login
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -15,7 +14,7 @@ import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-
+import company.locahost.itsc.dogetracker.R
 
 
 class Login : AppCompatActivity(), View.OnClickListener {
